d4c1714ee131a78af6418c9793b875b8501f64ab
Zachary Reiss <zachary.reiss123@gmail.com>
2019-08-30 00:53:15 -0600

    Add project files.

---
+++ BethanysPieShop/Models/IPieRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public interface IPieRepository
+    {
+        //return all pies
+        IEnumerable<Pie> GetAllPies();
+
+        //To get pies by their id
+        Pie GetPieById(int pieId);
+    }
+}
---
+++ BethanysPieShop/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Models;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace BethanysPieShop
+{
+    public class Program
+    {
+        //main method gets called at startup
+        public static void Main(string[] args)
+        {
+            //set up environment 
+            //CreateWebHostBuilder(args).Build().Run();
+
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var service = scope.ServiceProvider;
+                try
+                {
+                    var context = service.GetRequiredService<AppDbContext>();
+                    DbInitializer.Seed(context);
+                }
+                catch (Exception)
+                {
+                    //This would be logged in real world situation
+                }
+            }
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
